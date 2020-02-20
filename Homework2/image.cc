fdc90a6494ad16ec0dd49b22f1231bcee47c5ff6
Josh Winton <joshcwinton@gmail.com>
2020-02-19 19:55:33 -0500

    working image segmentation

--- Homework2/image.cc
+++ Homework2/image.cc
@@ -7,34 +7,27 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <string>
 #include <queue>
+#include <string>
 #include <utility>
 
 using namespace std;
 
-namespace ComputerVisionProjects
-{
+namespace ComputerVisionProjects {
 
-Image::Image(const Image &an_image)
-{
+Image::Image(const Image &an_image) {
   AllocateSpaceAndSetSize(an_image.num_rows(), an_image.num_columns());
   SetNumberGrayLevels(an_image.num_gray_levels());
 
   for (size_t i = 0; i < num_rows(); ++i)
-    for (size_t j = 0; j < num_columns(); ++j)
-    {
+    for (size_t j = 0; j < num_columns(); ++j) {
       SetPixel(i, j, an_image.GetPixel(i, j));
     }
 }
 
-Image::~Image()
-{
-  DeallocateSpace();
-}
+Image::~Image() { DeallocateSpace(); }
 
-void Image::AllocateSpaceAndSetSize(size_t num_rows, size_t num_columns)
-{
+void Image::AllocateSpaceAndSetSize(size_t num_rows, size_t num_columns) {
   if (pixels_ != nullptr)
     DeallocateSpace();
   pixels_ = new int *[num_rows];
@@ -45,8 +38,7 @@ void Image::AllocateSpaceAndSetSize(size_t num_rows, size_t num_columns)
   num_columns_ = num_columns;
 }
 
-void Image::DeallocateSpace()
-{
+void Image::DeallocateSpace() {
   for (size_t i = 0; i < num_rows_; i++)
     delete pixels_[i];
   delete pixels_;
@@ -55,21 +47,18 @@ void Image::DeallocateSpace()
   num_columns_ = 0;
 }
 
-bool ReadImage(const string &filename, Image *an_image)
-{
+bool ReadImage(const string &filename, Image *an_image) {
   if (an_image == nullptr)
     abort();
   FILE *input = fopen(filename.c_str(), "rb");
-  if (input == 0)
-  {
+  if (input == 0) {
     cout << "ReadImage: Cannot open file" << endl;
     return false;
   }
 
   // Check for the right "magic number".
   char line[1024];
-  if (fread(line, 1, 3, input) != 3 || strncmp(line, "P5\n", 3))
-  {
+  if (fread(line, 1, 3, input) != 3 || strncmp(line, "P5\n", 3)) {
     fclose(input);
     cout << "ReadImage: Expected .pgm file" << endl;
     return false;
@@ -92,13 +81,10 @@ bool ReadImage(const string &filename, Image *an_image)
   an_image->SetNumberGrayLevels(levels);
 
   // read pixel row by row.
-  for (int i = 0; i < num_rows; ++i)
-  {
-    for (int j = 0; j < num_columns; ++j)
-    {
+  for (int i = 0; i < num_rows; ++i) {
+    for (int j = 0; j < num_columns; ++j) {
       const int byte = fgetc(input);
-      if (byte == EOF)
-      {
+      if (byte == EOF) {
         fclose(input);
         cout << "ReadImage: short file" << endl;
         return false;
@@ -111,11 +97,9 @@ bool ReadImage(const string &filename, Image *an_image)
   return true;
 }
 
-bool WriteImage(const string &filename, const Image &an_image)
-{
+bool WriteImage(const string &filename, const Image &an_image) {
   FILE *output = fopen(filename.c_str(), "w");
-  if (output == 0)
-  {
+  if (output == 0) {
     cout << "WriteImage: cannot open file" << endl;
     return false;
   }
@@ -128,13 +112,10 @@ bool WriteImage(const string &filename, const Image &an_image)
   fprintf(output, "#\n");  // Empty comment.
   fprintf(output, "%d %d\n%03d\n", num_columns, num_rows, colors);
 
-  for (int i = 0; i < num_rows; ++i)
-  {
-    for (int j = 0; j < num_columns; ++j)
-    {
+  for (int i = 0; i < num_rows; ++i) {
+    for (int j = 0; j < num_columns; ++j) {
       const int byte = an_image.GetPixel(i, j);
-      if (fputc(byte, output) == EOF)
-      {
+      if (fputc(byte, output) == EOF) {
         fclose(output);
         cout << "WriteImage: could not write" << endl;
         return false;
@@ -150,31 +131,25 @@ bool WriteImage(const string &filename, const Image &an_image)
 // (adapted from J.D.Foley, A. van Dam, S.K.Feiner, J.F.Hughes
 // "Computer Graphics. Principles and practice",
 // 2nd ed., 1990, section 3.2.2);
-void DrawLine(int x0, int y0, int x1, int y1, int color,
-              Image *an_image)
-{
+void DrawLine(int x0, int y0, int x1, int y1, int color, Image *an_image) {
   if (an_image == nullptr)
     abort();
 
 #ifdef SWAP
 #undef SWAP
 #endif
-#define SWAP(a, b) \
-  {                \
-    a ^= b;        \
-    b ^= a;        \
-    a ^= b;        \
+#define SWAP(a, b)                                                             \
+  {                                                                            \
+    a ^= b;                                                                    \
+    b ^= a;                                                                    \
+    a ^= b;                                                                    \
   }
 
   const int DIR_X = 0;
   const int DIR_Y = 1;
 
   // Increments: East, North-East, South, South-East, North.
-  int incrE,
-      incrNE,
-      incrS,
-      incrSE,
-      incrN;
+  int incrE, incrNE, incrS, incrSE, incrN;
   int d;      /* the D */
   int x, y;   /* running coordinates */
   int mpCase; /* midpoint algorithm's case */
@@ -189,24 +164,19 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
   int dy = ymax - ymin;
   int dir;
 
-  if (dx * dx > dy * dy)
-  { // Horizontal scan.
+  if (dx * dx > dy * dy) { // Horizontal scan.
     dir = DIR_X;
-    if (xmax < xmin)
-    {
+    if (xmax < xmin) {
       SWAP(xmin, xmax);
       SWAP(ymin, ymax);
     }
     dx = xmax - xmin;
     dy = ymax - ymin;
 
-    if (dy >= 0)
-    {
+    if (dy >= 0) {
       mpCase = 1;
       d = 2 * dy - dx;
-    }
-    else
-    {
+    } else {
       mpCase = 2;
       d = 2 * dy + dx;
     }
@@ -214,25 +184,19 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
     incrNE = 2 * (dy - dx);
     incrE = 2 * dy;
     incrSE = 2 * (dy + dx);
-  }
-  else
-  { // vertical scan.
+  } else { // vertical scan.
     dir = DIR_Y;
-    if (ymax < ymin)
-    {
+    if (ymax < ymin) {
       SWAP(xmin, xmax);
       SWAP(ymin, ymax);
     }
     dx = xmax - xmin;
     dy = ymax - ymin;
 
-    if (dx >= 0)
-    {
+    if (dx >= 0) {
       mpCase = 1;
       d = 2 * dx - dy;
-    }
-    else
-    {
+    } else {
       mpCase = 2;
       d = 2 * dx + dy;
     }
@@ -247,26 +211,19 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
   y = ymin;
   done = 0;
 
-  while (!done)
-  {
+  while (!done) {
     an_image->SetPixel(x, y, color);
 
     // Move to the next point.
-    switch (dir)
-    {
+    switch (dir) {
     case DIR_X:
-      if (x < xmax)
-      {
-        switch (mpCase)
-        {
+      if (x < xmax) {
+        switch (mpCase) {
         case 1:
-          if (d <= 0)
-          {
+          if (d <= 0) {
             d += incrE;
             x++;
-          }
-          else
-          {
+          } else {
             d += incrNE;
             x++;
             y++;
@@ -274,39 +231,29 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
           break;
 
         case 2:
-          if (d <= 0)
-          {
+          if (d <= 0) {
             d += incrSE;
             x++;
             y--;
-          }
-          else
-          {
+          } else {
             d += incrE;
             x++;
           }
           break;
         }
-      }
-      else
-      {
+      } else {
         done = 1;
       }
       break;
 
     case DIR_Y:
-      if (y < ymax)
-      {
-        switch (mpCase)
-        {
+      if (y < ymax) {
+        switch (mpCase) {
         case 1:
-          if (d <= 0)
-          {
+          if (d <= 0) {
             d += incrE;
             y++;
-          }
-          else
-          {
+          } else {
             d += incrNE;
             y++;
             x++;
@@ -314,22 +261,18 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
           break;
 
         case 2:
-          if (d <= 0)
-          {
+          if (d <= 0) {
             d += incrSE;
             y++;
             x--;
-          }
-          else
-          {
+          } else {
             d += incrE;
             y++;
           }
           break;
         } // mpCase
       }   // y < ymin
-      else
-      {
+      else {
         done = 1;
       }
       break;
@@ -337,18 +280,12 @@ void DrawLine(int x0, int y0, int x1, int y1, int color,
   }
 }
 
-void ConvertGrayScaleToBinary(Image *an_image, int threshold)
-{
-  for (int i = 0; i < an_image->num_rows(); i++)
-  {
-    for (int j = 0; j < an_image->num_columns(); j++)
-    {
-      if (an_image->GetPixel(i, j) <= threshold)
-      {
+void ConvertGrayScaleToBinary(Image *an_image, int threshold) {
+  for (int i = 0; i < an_image->num_rows(); i++) {
+    for (int j = 0; j < an_image->num_columns(); j++) {
+      if (an_image->GetPixel(i, j) <= threshold) {
         an_image->SetPixel(i, j, 0);
-      }
-      else
-      {
+      } else {
         an_image->SetPixel(i, j, 255);
       }
     }
@@ -356,41 +293,34 @@ void ConvertGrayScaleToBinary(Image *an_image, int threshold)
   an_image->SetNumberGrayLevels(1);
 }
 
-// For each pixel in the image, check neighbors and based on their values, set new pixel value
-void LabelImage(Image *an_image)
-{
-  int next_label = 1;
+// For each pixel in the image, check neighbors and based on their values, set
+// new pixel value
+void LabelImage(Image *an_image) {
+  int current_label = 1;
   int labels[an_image->num_rows()][an_image->num_columns()];
   queue<pair<int, int>> label_queue;
 
   // initialize labels
-  for (int i = 0; i < an_image->num_rows(); i++)
-  {
-    for (int j = 0; j < an_image->num_columns(); j++)
-    {
+  for (int i = 0; i < an_image->num_rows(); i++) {
+    for (int j = 0; j < an_image->num_columns(); j++) {
       labels[i][j] = -1;
     }
   }
 
   // set labels
-  /**
-  for (int i = 0; i < an_image->num_rows(); i++)
-  {
-    for (int j = 0; j < an_image->num_columns(); j++)
-    {
+  for (int i = 0; i < an_image->num_rows(); i++) {
+    for (int j = 0; j < an_image->num_columns(); j++) {
       size_t current_val = an_image->GetPixel(i, j);
 
       // if foreground pixel and not already labelled
-      if (current_val && (labels[i][j] == -1))
-      {
+      if ((current_val > 0) && (labels[i][j] == -1)) {
         // give it current label and add it as the first element in a queue
-        labels[i][j] = next_label;
+        labels[i][j] = current_label;
         pair<int, int> pixel(i, j);
         label_queue.push(pixel);
       }
       // background pixel or already labeled
-      else
-      {
+      else {
         // go to next pixel
         continue;
       }
@@ -398,59 +328,72 @@ void LabelImage(Image *an_image)
       // 3
       // pop element from queue and look at its neighbors
 
-      while (!label_queue.empty())
-      {
+      while (!label_queue.empty()) {
         pair<int, int> queue_pixel(label_queue.front());
         label_queue.pop();
 
         int up;
-        int upLeft;
+        int down;
         int left;
+        int right;
 
-        if (queue_pixel.first > 0)
-        {
+        // check for up
+        if (queue_pixel.first > 0) {
           up = an_image->GetPixel(queue_pixel.first - 1, queue_pixel.second);
-          if (labels[queue_pixel.first - 1][queue_pixel.second] == -1)
-          {
-            labels[queue_pixel.first - 1][queue_pixel.second] = next_label;
+          if ((up > 0) &&
+              (labels[queue_pixel.first - 1][queue_pixel.second] == -1)) {
+            labels[queue_pixel.first - 1][queue_pixel.second] = current_label;
             pair<int, int> pixel(queue_pixel.first - 1, queue_pixel.second);
             label_queue.push(pixel);
           }
         }
 
-        if (queue_pixel.second > 0)
-        {
-          left = an_image->GetPixel(queue_pixel.first, queue_pixel.second - 1);
-          if (labels[queue_pixel.first][queue_pixel.second - 1] == -1)
-          {
-            labels[queue_pixel.first][queue_pixel.second - 1] = next_label;
-            pair<int, int> pixel(queue_pixel.first, queue_pixel.second - 1);
+        // check for down
+        if (queue_pixel.first < an_image->num_rows()-1){
+          down = an_image->GetPixel(queue_pixel.first + 1, queue_pixel.second);
+          if((down > 0) && (labels[queue_pixel.first+1][queue_pixel.second] == -1)){
+            labels[queue_pixel.first+1][queue_pixel.second] = current_label;
+            pair<int, int> pixel(queue_pixel.first+1, queue_pixel.second);
             label_queue.push(pixel);
           }
         }
 
-        if ((queue_pixel.first > 0) && (queue_pixel.second > 0))
-        {
-          upLeft = an_image->GetPixel(queue_pixel.first - 1, queue_pixel.second - 1);
-          if (labels[queue_pixel.first - 1][queue_pixel.second - 1] == -1)
-          {
-            labels[queue_pixel.first - 1][queue_pixel.second - 1] = next_label;
-            pair<int, int> pixel(queue_pixel.first - 1, queue_pixel.second - 1);
+
+        // check for left
+        if (queue_pixel.second > 0){
+          left = an_image->GetPixel(queue_pixel.first, queue_pixel.second-1);
+          if((left > 0) && (labels[queue_pixel.first][queue_pixel.second-1] == -1)){
+            labels[queue_pixel.first][queue_pixel.second-1] = current_label;
+            pair<int, int> pixel(queue_pixel.first, queue_pixel.second-1);
             label_queue.push(pixel);
           }
         }
 
-        next_label++;
+        // check for right
+        if (queue_pixel.second < an_image->num_columns()-1){
+          right = an_image->GetPixel(queue_pixel.first, queue_pixel.second+1);
+          if((right > 0) && (labels[queue_pixel.first][queue_pixel.second+1] == -1)){
+            labels[queue_pixel.first][queue_pixel.second+1] = current_label;
+            pair<int, int> pixel(queue_pixel.first, queue_pixel.second+1);
+            label_queue.push(pixel);
+          }
+        }
       }
+      current_label++;
     }
   }
-  */
-  for (int i = 0; i < an_image->num_rows(); i++)
-  {
-    for (int j = 0; j < an_image->num_columns(); j++)
-    {
+
+  an_image->SetNumberGrayLevels(current_label);
+
+  for (int i = 0; i < an_image->num_rows(); i++) {
+    for (int j = 0; j < an_image->num_columns(); j++) {
+      if(labels[i][j] == -1){
+        labels[i][j] = 0;
+      }
+      labels[i][j]*=25;
       an_image->SetPixel(i, j, labels[i][j]);
     }
   }
 }
+
 } // namespace ComputerVisionProjects
