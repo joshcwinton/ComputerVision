aab423876b575233aa6a158168bb194c8c9b1b66
joshcwinton <joshcwinton@gmail.com>
2020-02-21 22:22:21 -0500

    function headers,  members for database

--- Homework2/database.h
+++ Homework2/database.h
@@ -4,22 +4,48 @@
 #ifndef COMPUTER_VISION_DATABASE_H_
 #define COMPUTER_VISION_DATABASE_H_
 
+#include <string>
+#include <vector>
+#include <unordered_map>
 #include "image.h"
+#include "Object.h"
 
-namespace ComputerVisionProjects {
-class Database {
+using namespace std;
+
+typedef class ComputerVisionProjects::ObjectData object;
+
+namespace ComputerVisionProjects
+{
+class Database
+{
 public:
-  ~Database();
-  Database(const Image &labeled_image);
+  // ~Database();
+  Database(const Image *labeled_image);
 
-  vector<ObjectData> DatabaseData;
+  // Holds strings, each string corresponds to the characteristics of an object
+  vector<string> DatabaseData;
 
-  void BuildDatabaseFromLabeledImage(const Image &labeled_image);
-  void PrintDatabaseData();
+  // Stores vectors of pixel pairs corresponding to a label
+  unordered_map<int, vector<pair<int, int>>> objectMap;
 
-  size_t num_objects() const { return num_objects_; }
+  // Looks for objects labeled by color in labeled_image and inserts all
+  // pixels matching a label into a hash table indexed by label, objectMap
+  void BuildObjectMapFromLabeledImage(const Image *labeled_image);
 
-private:
-  size_t num_objects_;
-}
+  // Prints final output, all data for each object
+  // Attributes separated by spaces, objects separated by lines
+  // void PrintDatabaseData();
+
+  size_t num_objects() const
+  {
+    return objectMap.bucket_count();
+  };
+};
+
+// Takes a label and a map from labels to pixels
+// Computes
+int GetAverageRowOfObject(const int label, const unordered_map<int, vector<pair<int, int>>> objectMap);
+
 } // namespace ComputerVisionProjects
+
+#endif // COMPUTER_VISION_DATABASE_H
