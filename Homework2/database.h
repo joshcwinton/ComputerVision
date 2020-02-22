d8273651751aa9fed344b2d0cdfe391f4c2f1790
joshcwinton <joshcwinton@gmail.com>
2020-02-21 23:09:32 -0500

    added functions to get and print all labels

--- Homework2/database.h
+++ Homework2/database.h
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
 #include <unordered_map>
 #include "image.h"
 #include "Object.h"
@@ -18,15 +19,16 @@ namespace ComputerVisionProjects
 {
 class Database
 {
-public:
-  // ~Database();
-  Database(const Image *labeled_image);
-
+private:
   // Holds strings, each string corresponds to the characteristics of an object
   vector<string> DatabaseData;
 
   // Stores vectors of pixel pairs corresponding to a label
   unordered_map<int, vector<pair<int, int>>> objectMap;
+
+public:
+  // ~Database();
+  Database(const Image *labeled_image);
 
   // Looks for objects labeled by color in labeled_image and inserts all
   // pixels matching a label into a hash table indexed by label, objectMap
@@ -36,24 +38,51 @@ public:
   // Attributes separated by spaces, objects separated by lines
   // void PrintDatabaseData();
 
+  // Returns vector with all labels from image
+  vector<int> GetAllLabels()
+  {
+    vector<int> labels;
+
+    unordered_map<int, vector<pair<int, int>>>::iterator it = objectMap.begin();
+
+    while (it != objectMap.end())
+    {
+      labels.push_back(it->first);
+      it++;
+    }
+    return labels;
+  };
+
+  void PrintAllLabels()
+  {
+    vector<int> labels = GetAllLabels();
+    cout << "Labels:" << endl;
+    for (int label : labels)
+      cout << label << endl;
+  }
+
+  // Takes a label and a map from labels to pixels
+  // Computes average row value of all pixels in object
+  int GetAverageRowOfObject(int label);
+
+  // Takes a label and a map from labels to pixels
+  // Computes average column value of all pixels in object
+  int GetAverageColumnOfObject(int label);
+
   size_t num_objects() const
   {
     return objectMap.bucket_count();
   };
 };
 
-/* // Takes a label and a map from labels to pixels
-// Computes average row value of all pixels in object
-int GetAverageRowOfObject(const int label, const unordered_map<int, vector<pair<int, int>>> objectMap);
-
 // Takes a label and a map from labels to pixels
 // Computes average column value of all pixels in object
-int GetAverageColumnOfObject(const int label, const unordered_map<int, vector<pair<int, int>>> objectMap);
+// int GetAverageColumnOfObject(const int label, const unordered_map<int, vector<pair<int, int>>> objectMap);
 
 // Given a label and an unordered map
 // Find the minimum momemnt of inertia
 // int GetMinimumMomentOfInertia(const int label, const unordered_map<int, vector<pair<int, int>>> objectMap);
- */
+
 // namespace ComputerVisionProjects
 } // namespace ComputerVisionProjects
 
