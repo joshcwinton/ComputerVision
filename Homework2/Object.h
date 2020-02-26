e7b5682d8361304f9f99797cd5da105a312f927a
joshcwinton <joshcwinton@gmail.com>
2020-02-26 15:26:58 -0500

    object ?

---
+++ Homework2/Object.h
@@ -0,0 +1,20 @@
+// Class for storing object information for labeled binary image
+// Written by Joshua Winton for CSCI 49369 at Hunter College
+
+#ifndef COMPUTER_VISION_OBJECT_DATA_H_
+#define COMPUTER_VISION_OBJECT_DATA_H_
+
+#include "image.h"
+
+using namespace std;
+
+namespace ComputerVisionProjects
+{
+class ObjectData
+{
+public:
+private:
+};
+} // namespace ComputerVisionProjects
+
+#endif // COMPUTER_VISION_OBJECT_DATA_H
