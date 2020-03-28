6818137d9e536d77b2ac2cc0436cb42e6d971181
joshcwinton <joshcwinton@gmail.com>
2020-03-27 22:22:27 -0400

    changed header comment, input format

--- Homework4/s1.cc
+++ Homework4/s1.cc
@@ -1,6 +1,9 @@
-// Sample test file for the image class.
-// Reads a given pgm image, draws a line, and saves it to
-// another pgm image.
+/* 
+Author:  Josh Winton
+Project: Computer Vision Homework 4
+File:    s1.cc
+Purpose: Locates sphere in an image and computes its center and radius 
+*/
 #include "image.h"
 #include <cstdio>
 #include <iostream>
@@ -9,29 +12,24 @@
 using namespace std;
 using namespace ComputerVisionProjects;
 
-int
-main(int argc, char **argv){
-  
-  if (argc!=3) {
-    printf("Usage: %s file1 file2\n", argv[0]);
+int main(int argc, char **argv)
+{
+
+  if (argc != 4)
+  {
+    printf("Usage: %s [input original image] [input threshold value] [output parameters file]\n", argv[0]);
     return 0;
   }
-  const string input_file(argv[1]);
-  const string output_file(argv[2]);
+  const string input_image_file(argv[1]);
+  const string input_threshold(argv[2]);
+  const string output_parameters_file(argv[3]);
 
   Image an_image;
-  if (!ReadImage(input_file, &an_image)) {
-    cout <<"Can't open file " << input_file << endl;
+  if (!ReadImage(input_image_file, &an_image))
+  {
+    cout << "Can't open file " << input_image_file << endl;
     return 0;
   }
 
-  // Draw a line from (0, 0) to (100, 240) having gray-value
-  // 200.
-  DrawLine(0, 0, 100, 240, 200,
-	   &an_image); 
-  
-  if (!WriteImage(output_file, an_image)){
-    cout << "Can't write to file " << output_file << endl;
-    return 0;
-  }
+  // write parameters to file
 }
