08dd2736b5cedbe6e467759b7ca93c2b2bbc639b
joshcwinton <joshcwinton@gmail.com>
2020-03-27 23:50:22 -0400

    s1

--- Homework4/s1.cc
+++ Homework4/s1.cc
@@ -5,8 +5,10 @@ File:    s1.cc
 Purpose: Locates sphere in an image and computes its center and radius 
 */
 #include "image.h"
+#include "sphere.h"
 #include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -21,15 +23,37 @@ int main(int argc, char **argv)
     return 0;
   }
   const string input_image_file(argv[1]);
-  const string input_threshold(argv[2]);
+  const int input_threshold(stoi(argv[2]));
   const string output_parameters_file(argv[3]);
 
-  Image an_image;
+  printf("Input image file is \"%s\".\n", input_image_file.c_str());
+  printf("Input threshold is %i.\n", input_threshold);
+  printf("Output parameters file is \"%s\".\n", output_parameters_file.c_str());
+
+  Sphere an_image;
   if (!ReadImage(input_image_file, &an_image))
   {
     cout << "Can't open file " << input_image_file << endl;
     return 0;
   }
 
-  // write parameters to file
+  ThresholdImage(input_threshold, &an_image);
+
+  // (row, column)
+  pair<int, int> sphere_center = an_image.GetCenter();
+  int radius = an_image.GetRadius();
+
+  printf("Sphere center is (%i, %i).\n", sphere_center.first, sphere_center.second);
+  printf("Radius is %i.\n", radius);
+
+  ofstream output_file(output_parameters_file);
+  if (output_file.is_open())
+  {
+    output_file << sphere_center.second << " " << sphere_center.first << " " << radius << endl;
+    output_file.close();
+  }
+  else
+    cout << "Unable to open output file";
+
+  return 0;
 }
