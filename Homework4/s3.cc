fd32489fdb1a27862ba9214f89bffcea01439d0f
joshcwinton <joshcwinton@gmail.com>
2020-04-10 13:25:53 -0400

    s3

--- Homework4/s3.cc
+++ Homework4/s3.cc
@@ -1,4 +1,5 @@
-/* 
+
+/*
 Author:  Josh Winton
 Project: Computer Vision Homework 4
 File:    s3.cc
@@ -17,6 +18,18 @@ Purpose: Calculates surface normals and albedo using 3 images under different li
 
 using namespace std;
 using namespace ComputerVisionProjects;
+
+vector<int> makeVectorFromString(string vector_string)
+{
+  vector<int> vector_array;
+  istringstream ss(vector_string);
+  do  {
+    string word;
+    ss >> word;
+    vector_array.push_back(stoi(word));
+  } while (ss);
+  return vector_array;
+}
 
 int main(int argc, char **argv)
 {
@@ -42,7 +55,31 @@ int main(int argc, char **argv)
   printf("threshold: %i\n", threshold);
   printf("output_filename: \"%s\"\n", output_filename.c_str());
 
-  // Read input directions from file
+  // Read light source directions from file
+  vector<int> s1;              // lighting direction 1
+  vector<int> s2;              // lighting direction 2
+  vector<int> s3;              // lighting direction 2
+  vector<vector<int>> S[3][3]; // [s1, s2, s3]
+
+  string line;
+  ifstream input_directions_file(input_directions_filename);
+  if (input_directions_file.is_open())
+  {
+    getline(input_directions_file, line);
+    s1 = makeVectorFromString(line);
+    cout << s1[0] << ' ' << s1[1] << ' ' << s1[2] << endl;
+    getline(input_directions_file, line);
+    s2 = makeVectorFromString(line);
+
+    getline(input_directions_file, line);
+    s2 = makeVectorFromString(line);
+
+    input_directions_file.close();
+  }
+  else
+  {
+    printf("Unable to open file \"%s\"", input_directions_filename.c_str());
+  }
 
   // Read image files into Image objects
 
